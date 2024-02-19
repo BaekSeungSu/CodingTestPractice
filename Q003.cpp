69d5eb64588d21d3d43c9b75e144376f39e7f6ec
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-02-19 19:23:02 +0900

    Q3

---
+++ Q003.cpp
@@ -0,0 +1,28 @@
+// 백준 11659번
+// 구간 합 구하기 1
+
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+	int S[100001] = {};
+	for (int i = 1; i <= N; i++)
+	{
+		int temp;
+		cin >> temp;
+		S[i] = S[i - 1] + temp;
+	}
+	for (int i = 0; i < M; i++) {
+		int start, end;
+		cin >> start >> end;
+		cout << S[end] - S[start - 1] << '\n';
+	}
+
+}
