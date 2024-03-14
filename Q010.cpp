cb7008356ddeb6018da1d15370c3efda4a5af699
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-03-14 21:43:57 +0900

    슬라이딩 윈도우

---
+++ Q010.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+typedef pair<int, int> Node;
+
+int main()
+{
+	int N, L;
+	cin >> N >> L;
+	deque<Node> myDeque;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 0; i < N; i++) {
+		int now;
+		cin >> now;
+		while (myDeque.size() && myDeque.back().second > now) {
+			myDeque.pop_back();
+		}
+		myDeque.push_back(Node(i, now));
+		if (myDeque.front().first <= i - L) {
+			myDeque.pop_front();
+		}
+		cout << myDeque.front().second << '\n';
+	}
+}
