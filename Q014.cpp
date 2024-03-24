70a56753cf02b764e7123e6c6ac88bb1953cd981
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-03-25 07:43:17 +0900

    우선순위 큐 문제
    백준 11286번 : 절댓값 힙

---
+++ Q014.cpp
@@ -0,0 +1,50 @@
+// ¹éÁØ 11286 : Àý´ñ°ª Èü
+// priority_queue
+
+#include<iostream>
+#include<queue>
+#include<cmath>
+using namespace std;
+struct cmp {
+	bool operator()(int n1, int n2) {
+		if (abs(n1) > abs(n2))
+			return true;
+		else if (abs(n1) == abs(n2)) {
+			if (n1 > n2)
+				return true;
+			else
+				return false;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	priority_queue<int, vector<int>, cmp> pq;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		if (num == 0) {
+			if (pq.empty())
+				cout << "0" << "\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(num);
+		}
+	}
+
+	return 0;
+}
